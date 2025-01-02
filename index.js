7112c5ad5751c450632d6aed22caa0f5f64fe1e6
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-01-02 12:21:39 +0000

    generate

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const generateFourDigitRandomNumber = () => Math.floor(1000 + Math.random() * 9000)
+const generateRandomNumber = () => {
+  num = Math.floor(1000 + Math.random() * 9000)
+
+  str = String(num)
+
+  while (str.charAt(2) === '0') {
+    num = generateRandomNumber()
+  }
+
+  return num
+}
+
+const setNumberField = (event) => {
+  if (event) {
+    event.preventDefault()
+  }
+
+  document.getElementById('numberField').value = generateRandomNumber()
+}
+
+window.onload = () => {
+  setNumberField(null)
+}
